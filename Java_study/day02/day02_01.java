c88f842ad1084f4da3e33993b2d9e59e67417524
dongs1015 <stg1015@naver.com>
2026-09-11 13:46:04 +0900

    Add files via upload

---
+++ Java_study/day02/day02_01.java
@@ -0,0 +1,8 @@
+package day02;
+
+import java.lang.*;
+public class day02_01 {
+	public static void main(String args[]) {
+			System.out.println("이것은 이클립스로 작성한 문제"); }
+		
+	}
---
+++ Java_study/day02/day02_02.java
@@ -0,0 +1,9 @@
+package day02;
+
+import java.lang.*;
+
+public class day02_02 {
+	public static void main(String args[]) {
+		System.out.println("이름:홍길동\n나이:20살\n주소:서울");
+	}
+}
